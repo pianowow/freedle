33b6673b81df24815cc507e58692d35918c62921
Chris Irwin <pianowow@gmail.com>
2025-12-27 11:26:11 -0500

    fix: remove 'logic' from 4-letter word list

--- src/data/words.js
+++ src/data/words.js
@@ -1,5 +1,5 @@
 export const WORDS = {
-    4: ['code', 'plug', 'flow', 'grid', 'byte', 'data', 'link', 'logic', 'zoom', 'base'],
+    4: ['code', 'plug', 'flow', 'grid', 'byte', 'data', 'link', 'zoom', 'base'],
     5: ['react', 'vuejs', 'coder', 'logic', 'pixel', 'swift', 'cloud', 'frame', 'debug', 'alpha'],
     6: ['script', 'binary', 'syntax', 'module', 'string', 'buffer', 'socket', 'output', 'update', 'system']
 };
