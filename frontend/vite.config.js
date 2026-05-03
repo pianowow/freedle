0dc4fefee646b9934e02a5c598de718e1d14cbe8
Chris Irwin <pianowow@gmail.com>
2026-05-03 10:52:55 -0400

    add allowed origin for local development https testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/freedle/',
+  server: {
+    allowedHosts: ['freedle-dev.pianowow.com'],
+  },
   test: {
     environment: 'jsdom',
     globals: true,
