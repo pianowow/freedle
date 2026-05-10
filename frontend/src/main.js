e3898fc0550555e19c25558f7676f11eeb2ed4ef
Chris Irwin <pianowow@gmail.com>
2026-05-10 17:37:33 -0400

    css-colors phase 2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 
+document.documentElement.dataset.theme = "dark";
+
 const app = createApp(App);
 const pinia = createPinia();
 
